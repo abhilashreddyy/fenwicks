56c28860a6fe6f931918361d84e0e4f4aeaef63c
abhilashreddyy <abhilashreddyy1@gmail.com>
2019-09-23 23:50:40 +0530

    Update __init__.py

--- vision/models/__init__.py
+++ vision/models/__init__.py
@@ -2,5 +2,5 @@ import tensorflow as tf
 import keras_applications
 from tensorflow.python.keras import engine as keras_engine
 
-keras_applications.set_keras_submodules(backend=tf.keras.backend, layers=tf.keras.layers, models=tf.keras.models,
-                                        utils=tf.keras.utils, engine=keras_engine)
+#keras_applications.set_keras_submodules(backend=tf.keras.backend, layers=tf.keras.layers, models=tf.keras.models,
+#                                        utils=tf.keras.utils, engine=keras_engine)
